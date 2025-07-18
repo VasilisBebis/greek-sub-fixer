99685fc7f688a0fcb3e7b07621163c69fd80d817
VasilisBebis <e22113@unipi.gr>
2025-07-18 03:06:17 +0300

    read entire dir instead of single file

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,10 @@ use std::fs::{self, File};
 use encoding::{Encoding, DecoderTrap};
 use encoding::all::ISO_8859_7;
 use std::io::{Write, Result};
+use std::path::{PathBuf, Path};
+use std::ffi::OsStr;
 
-fn main() -> Result<()>{
-    let mut arguments = env::args_os();
-    //TODO: do better error detection
-    assert!(arguments.len() == 2); // TODO: temporary
-    arguments.next();
-    let binding = arguments.next().expect("sub file provided");
-    let sub_file = binding.to_str().unwrap();
-
+fn fix_sub_file(sub_file: &Path) -> Result<()> {
     let subs: Vec<u8> = fs::read(sub_file).unwrap();
 
     let utf8_subs = String::from(ISO_8859_7.decode(&subs, DecoderTrap::Replace).unwrap());
@@ -20,5 +15,30 @@ fn main() -> Result<()>{
     let mut file = File::create(sub_file)?;
     file.write_all(utf8_subs.as_bytes())?;
 
+    Ok(())
+}
+
+fn main() -> Result<()>{
+    let mut arguments = env::args_os();
+    //TODO: do better error detection
+    assert!(arguments.len() == 2); // TODO: temporary
+    arguments.next(); // skip program name
+    let path: PathBuf = PathBuf::from(arguments.next().unwrap());
+    if !path.is_dir() {
+        panic!("invalid directory provided");
+    }
+
+    for entry in path.read_dir().expect("reading failed") {
+        if let Ok(entry) = entry {
+            if let Some(extension) = entry.path().extension() {
+                if extension == "srt" {
+                    let result = fix_sub_file(&entry.path());
+                    if result.is_err() {
+                        panic!("error fixing file");
+                    }
+                }
+            }
+        }
+    }
     Ok(())
 }
